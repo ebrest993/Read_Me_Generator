4e7f29f59ff609676c1d237481148cd4b5a404ff
ebrest993 <elliott.brest@gmail.com>
2023-08-10 14:43:05 -0500

    "sample questions written.js"

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is your name?',
+    'What is your quest?',
+    'What is your favorite color?'
+];
 
+console.log(questions);
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
