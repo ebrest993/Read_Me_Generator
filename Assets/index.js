212e02cefa4bb107508f3db8299056340fef97d5
ebrest993 <elliott.brest@gmail.com>
2023-08-10 15:12:34 -0500

    test questions logged in console.js

--- Assets/index.js
+++ Assets/index.js
@@ -3,19 +3,29 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [
-    'What is your name?',
-    'What is your quest?',
-    'What is your favorite color?'
-];
+const questions = () => inquirer.prompt ([
+    {
+        name: 'test',
+        type: 'input',
+        message: [
+            'What is your name?',
+            'What is your quest?',
+            'What is your favorite color?'
+        ]
+    }        
+]);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+questions().then((answers) => {
+    console.log(answers);
 
-// TODO: Create a function to initialize app
-function init() {
-    console.log(questions);
-}
+})
 
-// Function call to initialize app
-init();
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {
+// }
+
+// // Function call to initialize app
+// init();
